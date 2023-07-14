a2cbfe4fdef227df500e6719ad3c5f1444319e58
Eann1S <danilio.ponomarev7766@gmail.com>
2023-07-14 18:39:29 +0400

    Move all configuration to Cloud Config Server
    - Moved configuration properties files with sensitive data to cloud config server
    - Added config server container to start up before tests
    - Pushed image with service to docker-hub

--- src/test/java/com/example/authentication/IntegrationTestBase.java
+++ src/test/java/com/example/authentication/IntegrationTestBase.java
@@ -3,6 +3,7 @@ package com.example.authentication;
 import com.example.authentication.dto.request.EmailRegisterRequest;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestComponent;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -19,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
@@ -44,10 +46,12 @@ public class IntegrationTestBase {
     @Container
     private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:alpine3.17"))
             .withExposedPorts(5432);
-
+    @Container
+    private static final GenericContainer<?> configServer = new GenericContainer<>(DockerImageName.parse("eann1s/cloud-config-server:1.0.0"))
+            .withExposedPorts(8888)
+            .waitingFor(Wait.forHttp("/authentication-service/test").forStatusCode(200));
     @Container
     private static final KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.3.2"));
-
 
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
@@ -57,12 +61,17 @@ public class IntegrationTestBase {
         registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
     }
 
+    @BeforeAll
+    static void setEnvironmentVariables() {
+        System.setProperty("CONFIG_SERVER_URL", configServer.getHost() + ":" + configServer.getFirstMappedPort());
+    }
+
     @TestComponent
     @Slf4j
     @Getter
     protected static class TestKafkaConsumerService {
 
-        private CountDownLatch latch = new CountDownLatch(1);
+        private final CountDownLatch latch = new CountDownLatch(1);
         private String messagePayload;
 
         @KafkaListener(topics = "${kafka.topics.authentication-service.producer}")
